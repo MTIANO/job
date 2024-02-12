206e2ce8f50bdcc86582a31d0909ffcd703a48d4
czw <chenzhongwei@kuaizi.ai>
2024-02-12 11:24:21 +0800

    test

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -32,6 +32,7 @@ class RouteServiceProvider extends ServiceProvider
             Route::middleware('api')
                 ->prefix('api')
                 ->group(base_path('routes/api.php'));
+
             Route::middleware('admin')
                 ->prefix('admin')
                 ->group(base_path('routes/admin.php'));
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    dump('会员的世界');
 });
 
 Route::get('/user', [\App\Http\Controllers\Controller::class, 'test']);
