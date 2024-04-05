f6672759b0ea006ac47e3fe6fd9458f6e01507c8
czw <chenzhongwei@kuaizi.ai>
2024-04-05 19:39:14 +0800

    微信验证

--- routes/web.php
+++ routes/web.php
@@ -19,11 +19,11 @@ Route::get('/', function () {
     dump('会员的世界');
 });
 
-Log::channel('daily')->info(11111);
+dump(1231231);die;
 Route::get('/user', [\App\Http\Controllers\Controller::class, 'test']);
 Route::match(['get', 'post'],'/', [WeiXinController::class,'index']);
 
-//Route::match(['get', 'post'],'/first_valid', [WeiXinController::class,'firstValid']);
-Route::post('/first_valid', [WeiXinController::class,'firstValid']);
-Route::get('/first_valid', [WeiXinController::class,'firstValid']);
+Route::match(['get', 'post'],'/first_valid', [WeiXinController::class,'firstValid']);
+//Route::post('/first_valid', [WeiXinController::class,'firstValid']);
+//Route::get('/first_valid', [WeiXinController::class,'firstValid']);
 
