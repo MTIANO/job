b079127e3f09a43c2f22ce2f00a46ae9d610d2ac
czw <chenzhongwei@kuaizi.ai>
2024-04-07 11:01:14 +0800

    微信验证

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,7 @@ Route::get('/', function () {
 });
 
 Route::get('/user', [\App\Http\Controllers\Controller::class, 'test']);
+Route::post('/first_valid', [WeiXinController::class,'firstValid']);
 Route::match(['get', 'post'],'/', [WeiXinController::class,'index']);
 
 Route::match(['get', 'post'],'/first_valid', [WeiXinController::class,'firstValid']);
