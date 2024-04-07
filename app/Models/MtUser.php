ce3f4923181cc143458701e4fc026c69eb65ac08
czw <chenzhongwei@kuaizi.ai>
2024-04-07 11:13:49 +0800

    微信验证

--- app/Models/MtUser.php
+++ app/Models/MtUser.php
@@ -3,11 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
-class MtUser extends Base
+class MtUser extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable;
 
     //protected $table = 'user';Base.php
     //const TABLE_NAME = 'user';
